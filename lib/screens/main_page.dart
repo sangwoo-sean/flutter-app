2ed17c0fa8bf9869cf5b58c069d002f3bc324961
Sangwoo Park <tkddn3367@gmail.com>
2021-04-27 17:35:59 +0900

    add showing saved words with Navigator change theme color with ThemeData

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 class MainPage extends StatelessWidget {
-
   MainPage({this.email});
 
   final String email;
@@ -12,9 +11,11 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+      ),
       home: RandomWords(),
     );
-
 
     // return Scaffold(
     //     appBar: AppBar(
@@ -35,7 +36,8 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+  final _suggestions = <WordPair>[]; //
+  final _saved = <WordPair>{}; // 저장될 세트
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   @override
@@ -43,6 +45,9 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [ // 리스트표시
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ]
       ),
       body: _buildSuggestions(),
     );
@@ -55,22 +60,68 @@ class _RandomWordsState extends State<RandomWords> {
           if (i.isOdd) return Divider(); // 홀수줄마다 디바이더로 칸나누기
 
           // final index = i ~/ 2; // 1, 2, 3, 4, 5 => 1//2, 2//2, 3//2, 4//2, 5//2 => 0, 1, 1, 2, 2 (단어가 pair이기때문에 실제 단어갯수를 표기해줌)
-          final index = i ~/ 1; // 1, 2, 3, 4, 5 => 1//2, 2//2, 3//2, 4//2, 5//2 => 0, 1, 1, 2, 2 (단어가 pair이기때문에 실제 단어갯수를 표기해줌)
+          final index = i ~/
+              1; // 1, 2, 3, 4, 5 => 1//2, 2//2, 3//2, 4//2, 5//2 => 0, 1, 1, 2, 2 (단어가 pair이기때문에 실제 단어갯수를 표기해줌)
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));  // 끝까지 내리면 10개 더 생성
+            _suggestions
+                .addAll(generateWordPairs().take(10)); // 끝까지 내리면 10개 더 생성
           }
           return _buildRow(_suggestions[index]);
         });
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final tiles = _saved.map(
+                    (WordPair pair) {
+                      return ListTile(
+                        title: Text(
+                          pair.asPascalCase,
+                          style: _biggerFont,
+                        ),
+                      );
+                    },
+            );
+            final divided = ListTile.divideTiles(
+              context: context,
+                tiles: tiles,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('좋아요'),
+              ),
+              body: ListView(children: divided,)
+            );
+          },
+      ),
+    );
+
+  }
 
 }
