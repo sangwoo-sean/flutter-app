899f1a5db3dc7ade99032b296dee22c663edc50c
Sangwoo Park <tkddn3367@gmail.com>
2021-05-02 22:24:01 +0900

    add drawer

--- lib/screens/another.dart
+++ lib/screens/another.dart
@@ -4,6 +4,27 @@ class anotherRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(child: Text('Drawer Header'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            ),
+            ListTile(
+              title: Text("Item1"),
+            ),
+            ListTile(
+              title: Text("close"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("another Route"),
       ),
